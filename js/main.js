c3c808d7cfeea910e32540536e44d489fc2eed97
kristinejlu <kristinejlu@gmail.com>
2022-03-11 13:57:09 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    seen: true,
+  },
+});
+
+app.seen = false;
---
+++ js/publications.js
@@ -0,0 +1,7 @@
+const pubs = [
+  {
+    title: '',
+    authors: ['Chirstine Alvarado', 'Gustavo Umbelino', 'Mia Minnes'],
+    url: '',
+  },
+];
